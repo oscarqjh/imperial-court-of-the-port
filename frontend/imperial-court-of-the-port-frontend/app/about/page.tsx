6bb70c379f5733367a97149d4942939a080f2a4a
ykIsCoding <ylim189@e.ntu.edu.sg>
2025-10-19 11:05:41 +0800

    added about page

--- frontend/imperial-court-of-the-port-frontend/app/about/page.tsx
+++ frontend/imperial-court-of-the-port-frontend/app/about/page.tsx
@@ -1,10 +1,11 @@
+import ImperialCourtIntro from "@/components/imperial-court-intro";
 import Information from "@/components/information";
 import JobDashboard from "@/components/job-dashboard";
 
 export default function AboutPage() {
   return (
     <main className="">
-      <Information/>
+      <ImperialCourtIntro/>
     </main>
   );
 }
