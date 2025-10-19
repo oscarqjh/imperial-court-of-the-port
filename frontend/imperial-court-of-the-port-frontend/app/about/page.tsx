4a78cbf12c178058255e0bd1d9e084e7d6b3760b
oscarqjh <oscar.jh9@gmail.com>
2025-10-19 11:44:41 +0800

    Refactor AboutPage component for improved formatting and readability

--- frontend/imperial-court-of-the-port-frontend/app/about/page.tsx
+++ frontend/imperial-court-of-the-port-frontend/app/about/page.tsx
@@ -1,13 +1,11 @@
 import { ChatHeader } from "@/components/chat-header";
 import ImperialCourtIntro from "@/components/imperial-court-intro";
-import Information from "@/components/information";
-import JobDashboard from "@/components/job-dashboard";
 
 export default function AboutPage() {
   return (
     <main className="">
-      <ChatHeader/>
-      <ImperialCourtIntro/>
+      <ChatHeader />
+      <ImperialCourtIntro />
     </main>
   );
 }
